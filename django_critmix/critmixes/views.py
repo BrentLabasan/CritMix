221d3d1bab2c92454cedc29cbd3b57e93a8fea18
Brent Labasan <brentlabasan@gmail.com>
2012-11-25 16:50:03 -0800

    SoundCloud Python SDK integrated, current position of track is displayed in a span

--- django_critmix/critmixes/views.py
+++ django_critmix/critmixes/views.py
@@ -1,5 +1,8 @@
 from critmixes.models import Critmix
 from django.shortcuts import render_to_response, get_object_or_404
+import soundcloud
+
+client = soundcloud.Client(client_id='4d9fea6bff39bf127b10e87a2f478aea')
 
 def index(request):
     latest_critmix_list = Critmix.objects.all()
@@ -7,7 +10,15 @@ def index(request):
 
 def detail(request, critmix_id):
     p = get_object_or_404(Critmix, pk=critmix_id)
-    return render_to_response('critmixes/detail.html', {'critmix': p})
+    #print "p is", p
+    # get a tracks oembed data
+    #track_url = 'http://soundcloud.com/forss/flickermood'
+    embed_info = client.get('/oembed', url=p)
+
+    # print the html for the player widget
+    #print embed_info.html
+
+    return render_to_response('critmixes/detail.html', {'critmix': p, 'embed_info': embed_info.html})
 
 def results(request, critmix_id):
     return HttpResponse("You're looking at the results of critmix %s." % critmix_id)
