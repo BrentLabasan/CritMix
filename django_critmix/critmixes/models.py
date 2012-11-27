cf0a1de8186d587fb6655dc49765bccd4ac3b687
brent <brent@brent-laptop.(none)>
2012-11-27 13:57:41 -0800

    views.py SC code added

--- django_critmix/critmixes/models.py
+++ django_critmix/critmixes/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-import soundcloud
-
-client = soundcloud.Client(client_id='4d9fea6bff39bf127b10e87a2f478aea')
 
 # Create your models here.
 
--- django_critmix/critmixes/views.py
+++ django_critmix/critmixes/views.py
@@ -1,5 +1,8 @@
 from critmixes.models import Critmix
 from django.shortcuts import render_to_response, get_object_or_404
+import soundcloud
+# create a client object with your app credentials
+client = soundcloud.Client(client_id='4d9fea6bff39bf127b10e87a2f478aea')
 
 def index(request):
     latest_critmix_list = Critmix.objects.all()
@@ -7,6 +10,11 @@ def index(request):
 
 def detail(request, critmix_id):
     p = get_object_or_404(Critmix, pk=critmix_id)
+    # get a tracks oembed data
+    track_url = p
+    embed_info = client.get('/oembed', url=track_url)
+    # print the html for the player widget
+    print embed_info
     return render_to_response('critmixes/detail.html', {'critmix': p})
 
 def results(request, critmix_id):
